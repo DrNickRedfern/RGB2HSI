3b840ee38169f3dd0a5fedb5cace238c9c5809a6
DrNickRedfern <71695111+DrNickRedfern@users.noreply.github.com>
2024-05-12 21:23:44 +0100

    added .gitignore

--- RGB2HSI.py
+++ RGB2HSI.py
@@ -1,30 +1,39 @@
-# input is a tuple of RGB colour values
-# example: RGB2HSI((100, 150, 200))
-
-def RGB2HSI ( colour ): 
+def RGB2HSI (colour: tuple[int | float, int | float, int | float]) -> tuple[int, float, int]:
+    """
+    Converts an RGB color tuple to an HSI (Hue, Saturation, Intensity) tuple.
+    
+    Args:
+        colour (tuple[int | float, int | float, int | float]): A tuple of RGB color values.
+    
+    Returns:
+        tuple[int, float, int]: A tuple containing the Hue (0-360), Saturation (0-100), and Intensity (0-255) values.
+        
+    Examples:
+        RGB2HSI((100, 150, 200))
+        (210, 33.3, 150)
+    """
     
     from math import acos, pi, sqrt
     
     (R, G, B) = colour
        
-    r = R / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001)) 
-    g = G / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
-    b = B / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
+    r: float = R / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001)) 
+    g: float = G / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
+    b: float = B / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
    
-    num = 0.5 * ((r - g) + (r - b))
-    den = sqrt((r - g)**2 + (r - b) * (g - b))
-    h = acos(num / (den + 0.0000001))
+    num: float = 0.5 * ((r - g) + (r - b))
+    den: float = sqrt((r - g)**2 + (r - b) * (g - b))
+    h: float = acos(num / (den + 0.0000001))
     
     if b <= g:
-        h = h
+        h: float = h
     else:
-        h = 2 * pi - h
+        h: float = 2 * pi - h
     
-    H = round(h * 180/pi)
+    H: int = round(h * 180/pi)
     
-    S = round(100 * (1 - 3 * min(r, g, b) + 0.000001), 1)
+    S: float = round(100 * (1 - 3 * min(r, g, b) + 0.000001), 1)
     
-    I = round((R + G + B)/3)
+    I: int = round((R + G + B)/3)
          
     return (H, S, I)
-
--- deltaHSI.py
+++ deltaHSI.py
@@ -1,23 +1,35 @@
-# inputs are tuples of HSI colour values
-# example: deltaHSI((210, 33.3, 150), (139, 57.1, 117))
-
-def deltaHSI (colour1, colour2): 
+def deltaHSI (colour1: tuple[int | float, int | float, int | float], 
+              colour2: tuple[int | float, int | float, int | float]
+              ) -> float: 
+    """
+    Calculates the delta (difference) between two HSI color values.
     
+    Args:
+        colour1 (tuple[int | float, int | float, int | float]): The first HSI color value.
+        colour2 (tuple[int | float, int | float, int | float]): The second HSI color value.
+    
+    Returns:
+        float: The delta (difference) between the two HSI color values.
+        
+    Example:
+        eltaHSI((210, 33.3, 150), (139, 57.1, 117))
+        81.45
+    """
+        
     from math import cos, pi, sqrt
     
     (H1, S1, I1) = colour1
     (H2, S2, I2) = colour2
     
-    delta_I = abs(I1 - I2)
+    delta_I: int | float = abs(I1 - I2)
     
     if abs(H1 - H2) <= pi:
-        t = abs(H1 - H2)
+        t: int | float = abs(H1 - H2)
     else:
-        t = 2 * pi - abs(H1 - H2)
+        t: float = 2 * pi - abs(H1 - H2)
     
-    delta_C = sqrt( S1**2 + S2**2 - (2 * S1 * S2 * cos(t)) )
+    delta_C: float = sqrt( S1**2 + S2**2 - (2 * S1 * S2 * cos(t)) )
     
-    delta_HSI = sqrt(delta_I**2 + delta_C**2)
+    delta_HSI: float = sqrt(delta_I**2 + delta_C**2)
     
     return delta_HSI
-
